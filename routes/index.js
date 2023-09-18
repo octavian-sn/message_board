833b95f1c9b2a41f600a2935933b5ff75031844a
Octavian Sulic <octavius1992@gmail.com>
2023-09-18 14:40:59 +0300

    Add form view and handler

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,24 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+  res.render('index', { title: 'Mini MessageBoard', messages: messages });
+});
+
+// New message
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+// Handle message submit
+router.post('/new', function(req, res, next) {
+  const newMessage ={
+    text: req.body.message,
+    user: req.body.name || "Anonymous",
+    added: new Date().toLocaleTimeString()
+  }
+  messages.push(newMessage);
+  res.render('index', { title: 'Mini MessageBoard', messages: messages });
+
 });
 
 module.exports = router;
